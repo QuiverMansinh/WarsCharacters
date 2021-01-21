039034dbff94eb7b830fae74f9f18991650f805a
Matthew <sshadow.230701@gmail.com>
2021-01-22 12:42:22 +1300

    Itemz & other stuff

--- app/src/main/java/com/example/imperialassault/Item_Select_Screen.kt
+++ app/src/main/java/com/example/imperialassault/Item_Select_Screen.kt
@@ -1,20 +1,119 @@
 package com.example.imperialassault
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_item__select__screen.*
 
 class Item_Select_Screen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item__select__screen)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        val adapter = MyAdapter(this, supportFragmentManager,tab_layout.tabCount)
+        pager.adapter = adapter
+        pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    pager.currentItem = tab.position
+                }
+            }
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+}
+
+@Suppress("DEPRECATION")
+internal class MyAdapter(
+    var context: Context,
+    fm: FragmentManager,
+    var totalTabs: Int
+) :
+    FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                Rewards()
+            }1 -> {
+                Armor()
+            }2 -> {
+                Drones()
+            }3 -> {
+                Melee()
+            }4 -> {
+                Ranged()
+            }
+            else -> getItem(position)
         }
+    }
+    override fun getCount(): Int {
+        return totalTabs
+    }
+}
+
+class Rewards : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.rewards_fragment,container,false)
+    }
+}
+
+class Drones : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.drones_fragment,container,false)
+    }
+}
+
+class Armor : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.armor_fragment,container,false)
+    }
+}
+
+class Melee : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.melee_fragment,container,false)
+    }
+}
+
+class Ranged : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.ranged_fragment,container,false)
     }
 }
