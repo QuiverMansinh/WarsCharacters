844120b4e8f60a687c3fee24a575b25e53960169
Matthew <sshadow.230701@gmail.com>
2021-01-30 21:57:27 +1300

    Мэнни's dreams становятся true

--- app/src/main/java/com/example/imperialassault/Item_Select_Screen.kt
+++ app/src/main/java/com/example/imperialassault/Item_Select_Screen.kt
@@ -7,21 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_item__select__screen.*
 import kotlinx.android.synthetic.main.list_item.view.*
-import kotlinx.android.synthetic.main.rewards_fragment.*
-import kotlinx.android.synthetic.main.rewards_fragment2.*
 
 class Item_Select_Screen : AppCompatActivity() {
 
@@ -91,9 +84,36 @@ class Rewards constructor() : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var rewardsPathIds = arrayListOf<Int>(
+            R.drawable.reward1,
+            R.drawable.reward2,
+            R.drawable.reward3,
+            R.drawable.reward4,
+            R.drawable.reward5,
+            R.drawable.reward6,
+            R.drawable.reward7,
+            R.drawable.reward8,
+            R.drawable.reward9,
+            R.drawable.reward10,
+            R.drawable.reward11,
+            R.drawable.reward12,
+            R.drawable.reward13,
+            R.drawable.reward14,
+            R.drawable.reward15,
+            R.drawable.reward16,
+            R.drawable.reward17,
+            R.drawable.reward18,
+            R.drawable.reward19,
+            R.drawable.reward20,
+            R.drawable.reward21,
+            R.drawable.reward22,
+            R.drawable.reward23,
+            R.drawable.reward24,
+            R.drawable.reward25,
+        )
         val rewardsView = inflater.inflate(R.layout.rewards_fragment2,container,false) as View
         val rewardsgrid = rewardsView.findViewById<ImageView>(R.id.rewards_grid) as GridView
-        rewardsgrid.adapter = ImageAdapter(this.context as Activity)
+        rewardsgrid.adapter = ImageAdapter(this.context as Activity,rewardsPathIds)
         return rewardsView
     }
 }
@@ -104,7 +124,42 @@ class Drones : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.drones_fragment,container,false)
+        var rewardsPathIds = arrayListOf<Int>(
+            -6,
+            -1,
+            -6,
+            R.drawable.acc_t1_bactapump,
+            R.drawable.acc_t1_combatvembrace,
+            R.drawable.acc_t1_emergancyinjector,
+            R.drawable.acc_t1_portablemedkit,
+            R.drawable.acc_t1_survivalgear,
+            -5,
+            -6,
+            -2,
+            -6,
+            R.drawable.acc_t2_cyberneticarm,
+            R.drawable.acc_t2_extraammunition,
+            R.drawable.acc_t2_mandalorianhelmet,
+            R.drawable.acc_t2_r5_astromech,
+            R.drawable.acc_t2_slicingtools,
+            -5,
+            -6,
+            -3,
+            -6,
+            R.drawable.acc_t3_combatknife,
+            R.drawable.acc_t3_combatvisor,
+            R.drawable.acc_t3_concussiongrenades,
+            R.drawable.acc_t3_hiddenblade,
+            R.drawable.acc_t3_personalshield,
+            R.drawable.acc_t3_powercharger,
+            R.drawable.acc_t3_reinforcedhelmet,
+            R.drawable.acc_t3_supplypack,
+            -5,
+        )
+        val rewardsView = inflater.inflate(R.layout.rewards_fragment2,container,false) as View
+        val rewardsgrid = rewardsView.findViewById<ImageView>(R.id.rewards_grid) as GridView
+        rewardsgrid.adapter = ImageAdapter(this.context as Activity,rewardsPathIds)
+        return rewardsView
     }
 }
 
@@ -114,7 +169,30 @@ class Armor : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.armor_fragment,container,false)
+        var rewardsPathIds = arrayListOf<Int>(
+            -6,
+            -1,
+            -6,
+            R.drawable.arm_t1_combatcoat,
+            R.drawable.arm_t1_responsivearmor,
+            R.drawable.arm_t1_shadowsilkcloak,
+            -6,
+            -2,
+            -6,
+            R.drawable.arm_t2_combatcoat,
+            R.drawable.arm_t2_environmenthazardsuit,
+            R.drawable.arm_t2_laminatearmor,
+            -6,
+            -3,
+            -6,
+            R.drawable.arm_t3_admiralsuniform,
+            R.drawable.arm_t3_laminatearmor,
+            R.drawable.arm_t3_plastoidarmor,
+        )
+        val rewardsView = inflater.inflate(R.layout.rewards_fragment2,container,false) as View
+        val rewardsgrid = rewardsView.findViewById<ImageView>(R.id.rewards_grid) as GridView
+        rewardsgrid.adapter = ImageAdapter(this.context as Activity,rewardsPathIds)
+        return rewardsView
     }
 }
 
@@ -124,7 +202,60 @@ class Melee : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.melee_fragment,container,false)
+        var rewardsPathIds = arrayListOf<Int>(
+            -6,
+            -1,
+            -6,
+            R.drawable.mel_t1_armoredgauntlets,
+            R.drawable.mel_t1_gaffistick,
+            R.drawable.mel_t1_punchdagger,
+            R.drawable.mel_t1_vibroblade,
+            R.drawable.mel_t1_vibroknife,
+            R.drawable.mel_t1_vibrosword,
+            -6,
+            -4,
+            -6,
+            R.drawable.mod_t1_balancedhilt,
+            R.drawable.mod_t1_extendedhaft,
+            -5,
+            -6,
+            -2,
+            -6,
+            R.drawable.mel_t2_bd_1vibroax,
+            R.drawable.mel_t2_doublevibrosword,
+            R.drawable.mel_t2_polearm,
+            R.drawable.mel_t2_stunbaton,
+            R.drawable.mel_t2_vibroknucklers,
+            -5,
+            -6,
+            -4,
+            -6,
+            R.drawable.mod_t2_energizedhilt,
+            R.drawable.mod_t2_highimpactguard,
+            R.drawable.mod_t2_weightedhead,
+            R.drawable.mod_t2_focusingbeam,
+            -5,
+            -5,
+            -6,
+            -3,
+            -6,
+            R.drawable.mel_t3_ancientlightsaber,
+            R.drawable.mel_t3_borifle,
+            R.drawable.mel_t3_electrostaff,
+            R.drawable.mel_t3_forcepike,
+            R.drawable.mel_t3_ryykblades,
+            -5,
+            -6,
+            -4,
+            -6,
+            R.drawable.mod_t3_shockemitter,
+            R.drawable.mod_t3_vibrogenerator,
+            -5,
+        )
+        val rewardsView = inflater.inflate(R.layout.rewards_fragment2,container,false) as View
+        val rewardsgrid = rewardsView.findViewById<ImageView>(R.id.rewards_grid) as GridView
+        rewardsgrid.adapter = ImageAdapter(this.context as Activity,rewardsPathIds)
+        return rewardsView
     }
 }
 
@@ -134,69 +265,124 @@ class Ranged : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.ranged_fragment,container,false)
+        var rewardsPathIds = arrayListOf<Int>(
+            -6,
+            -1,
+            -6,
+            R.drawable.gun_t1_chargedpistol,
+            R.drawable.gun_t1_ddcdefender,
+            R.drawable.gun_t1_dh17,
+            R.drawable.gun_t1_dl44,
+            R.drawable.gun_t1_e11,
+            R.drawable.gun_t1_handcannon,
+            R.drawable.gun_t1_tatooinehuntingrifle,
+            -5,
+            -5,
+            -6,
+            -4,
+            -6,
+            R.drawable.mod_t1_marksmanbarrel,
+            R.drawable.mod_t1_sniperscope,
+            R.drawable.mod_t1_tacticaldisplay,
+            R.drawable.mod_t1_underbarrelhh4,
+            R.drawable.mod_t1_chargedammopack,
+            -5,
+            -6,
+            -2,
+            -6,
+            R.drawable.gun_t2_434deathhammer,
+            R.drawable.gun_t2_a280,
+            R.drawable.gun_t2_dt12heavyblasterpistol,
+            R.drawable.gun_t2_e11d,
+            R.drawable.gun_t2_ee3carbine,
+            R.drawable.gun_t2_huntersrifle,
+            R.drawable.gun_t2_t21,
+            -5,
+            -5,
+            -6,
+            -4,
+            -6,
+            R.drawable.mod_t2_boltupgrade,
+            R.drawable.mod_t2_overcharger,
+            R.drawable.mod_t2_plasmacell,
+            R.drawable.mod_t2_spreadbarrel,
+            -5,
+            -5,
+            -6,
+            -3,
+            -6,
+            R.drawable.gun_t3_a12sniperrifle,
+            R.drawable.gun_t3_disruptorpistol,
+            R.drawable.gun_t3_dlt19,
+            R.drawable.gun_t3_dxr6,
+            R.drawable.gun_t3_modifiedenergycannon,
+            R.drawable.gun_t3_pulsecannon,
+            R.drawable.gun_t3_sportingblaster,
+            R.drawable.gun_t3_valken38carbine,
+            -5,
+            -6,
+            -4,
+            -6,
+            R.drawable.mod_t3_disruptioncell,
+            R.drawable.mod_t3_telescopingsights,
+            -5,
+        )
+        val rewardsView = inflater.inflate(R.layout.rewards_fragment2,container,false) as View
+        val rewardsgrid = rewardsView.findViewById<ImageView>(R.id.rewards_grid) as GridView
+        rewardsgrid.adapter = ImageAdapter(this.context as Activity,rewardsPathIds)
+        return rewardsView
     }
 }
 
-class ImageAdapter internal constructor(val mContext: Activity) : BaseAdapter() {
+class ImageAdapter internal constructor(val mContext: Activity, var imageArray:
+ArrayList<Int>) :
+    BaseAdapter
+    () {
 
     // References to our images
-    val mThumbIds = arrayOf(
-        R.drawable.reward1,
-        R.drawable.reward2,
-        R.drawable.reward3,
-        R.drawable.reward4,
-        R.drawable.reward5,
-        R.drawable.reward6,
-        R.drawable.reward7,
-        R.drawable.reward8,
-        R.drawable.reward9,
-        R.drawable.reward10,
-        R.drawable.reward11,
-        R.drawable.reward12,
-        R.drawable.reward13,
-        R.drawable.reward14,
-        R.drawable.reward15,
-        R.drawable.reward16,
-        R.drawable.reward17,
-        R.drawable.reward18,
-        R.drawable.reward19,
-        R.drawable.reward20,
-        R.drawable.reward21,
-        R.drawable.reward22,
-        R.drawable.reward23,
-        R.drawable.reward24,
-        R.drawable.reward25,
-    )
+
     override fun getCount(): Int {
-        return mThumbIds.size
+        return imageArray.size
     }
 
     override fun getItem(position: Int): Any? {
-        return null
+        return position
     }
 
     override fun getItemId(position: Int): Long {
-        return 0
+        return position.toLong()
     }
-
-
     // Create a new ImageView for each item referenced by the Adapter
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var imageView: ImageView
         var gridItem: View
 
-        if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            val inflater = mContext.layoutInflater
-            gridItem = inflater.inflate(R.layout.list_item, null, true)
-            gridItem.reward1.setImageResource(mThumbIds[position])
-        } else {
-            gridItem = (convertView as View?)!!
+        if (imageArray[position] > 0){
+            gridItem = mContext.layoutInflater.inflate(R.layout.list_item, null, true)
+            gridItem.item.setImageResource(imageArray[position])
+        }else if (imageArray[position] == -1){
+            gridItem = mContext.layoutInflater.inflate(R.layout.tier1_title, null, true)
+        }else if (imageArray[position] == -2){
+            gridItem = mContext.layoutInflater.inflate(R.layout.tier2_title, null, true)
+        }else if (imageArray[position] == -3){
+            gridItem = mContext.layoutInflater.inflate(R.layout.tier3_title, null, true)
+        }else if (imageArray[position] == -4){
+            gridItem = mContext.layoutInflater.inflate(R.layout.mods_divider, null, true)
+        }else if (imageArray[position] == -5){
+            gridItem = mContext.layoutInflater.inflate(R.layout.empty_item, null, true)
+        }else {
+            gridItem = mContext.layoutInflater.inflate(R.layout.empty_space, null, true)
         }
         return gridItem
     }
 }
 
-
-
+class GlideryBullSh constructor(var idArrayList: ArrayList<Int>, var pathToDrawable:
+ArrayList<Int>,val activity: Fragment ,val fragment: View) {
+    fun doYourThing(){
+        for(i in 0..idArrayList.size-1){
+            Glide.with(activity)
+                .load(pathToDrawable[i])
+                .into(fragment.findViewById(idArrayList[i]))
+        }
+    }
+}
